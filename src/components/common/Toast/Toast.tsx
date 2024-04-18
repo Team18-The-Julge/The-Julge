e2cde3e4740c2e9ced7bdfe35aaeaf41a8afe9a8
KimTaeEun1632 <qpwoal1324@naver.com>
2024-04-18 21:25:25 +0900

    JSDoc추가

--- src/components/common/Toast/Toast.tsx
+++ src/components/common/Toast/Toast.tsx
@@ -3,6 +3,12 @@ import { useEffect } from 'react';
 import { ToastProps } from '@/components/common/Toast/Type-Toast';
 import UiToast from '@/components/common/Toast/Ui-Toast/Ui-Toast';
 
+/**
+ * Toast 컴포넌트
+ * @param onShow setShowToast를 (false)로 변경합니다.
+ * @param children, children으로 받아온 내용을 보여줍니다.
+ */
+
 function Toast({ onShow, children }: ToastProps) {
   useEffect(() => {
     const showToastTimer = setTimeout(() => {
